da15a9ec7ba6e230a3927d1120f0bcc478003073
guoyk <982685063@qq.com>
2021-04-30 14:45:34 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 5000choyen
 适用hoshino的5000兆円欲しい! style图片生成器插件
+
+在SAGIRI-kawaii大佬的插件（https://github.com/SAGIRI-kawaii/saya_plugins_collection/tree/master/modules/5000zhao）基础上修改而来的适用hoshino的5000兆円欲しい! style图片生成器插件。
+
+字体文件以防万一就不放了，可以自己找喜欢的用，替换generator.py下upper_font_path和downer_font_path文件名即可。想贴近原版风格的话上半句选粗黑体，下半句选粗宋体）
+
+使用方法：发送“5000兆元 (上半句)|(下半句)”
---
+++ __init__.py
@@ -0,0 +1,33 @@
+from PIL import Image
+
+from hoshino import Service, priv, logger, aiorequests
+from hoshino.typing import CQEvent, MessageSegment
+from hoshino.util import FreqLimiter, DailyNumberLimiter, pic2b64
+
+from .generator import genImage
+
+lmt = DailyNumberLimiter(10)
+
+sv = Service('5000choyen', help_='[5000兆元] (上半句)|(下半句)')
+
+@sv.on_prefix(('5000兆元','5000兆円'))
+async def gen_5000_pic(bot, ev: CQEvent):
+    uid = ev.user_id
+    gid = ev.group_id
+    mid= ev.message_id
+    if not lmt.check(uid):
+        await bot.send(ev, f'您今天已经使用过10次生成器了，休息一下明天再来吧~', at_sender=True)
+        return
+    try:
+        keyword = ev.message.extract_plain_text().strip()
+        if not keyword:
+            await bot.send(ev, '请提供要生成的句子！')
+            return
+        upper=keyword.split("|")[0]
+        downer=keyword.split("|")[1]
+        img=genImage(word_a=upper, word_b=downer)
+        img = str(MessageSegment.image(pic2b64(img)))
+        await bot.send(ev, img)
+        lmt.increase(uid)
+    except:
+        await bot.send(ev, '生成失败……请检查命令格式是否正确')
